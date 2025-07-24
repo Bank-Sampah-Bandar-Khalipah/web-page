49ee492faff8bdd90a455e7e495bd6949f4463ff
YusufKaisar <ykaisar10@gmail.com>
2025-07-24 09:50:25 +0700

    update beberapa tambahan konten

---
+++ src/Components/AboutPage/GaleryAbout.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Img1 from '../../assets/images/manfaatpage/bandarkhalipah.webp'
+import Img2 from '../../assets/images/manfaatpage/bandarkhalipah2.webp'
+import Img3 from '../../assets/images/manfaatpage/bandarkhalipah3.webp'
+import Img4 from '../../assets/images/manfaatpage/bandarkhalipah4.webp'
+import Img5 from '../../assets/images/manfaatpage/bandarkhalipah5.webp'
+import Img6 from '../../assets/images/manfaatpage/bandarkhalipah6.webp'
+import Img7 from '../../assets/images/manfaatpage/bandarkhalipah7.webp'
+
+
+const GaleryAbout = () => {
+  return (
+    <section className='container mx-auto section-spacing'>
+        <div className='space-y-10'>
+            <h1 className='text-3xl font-bold'>Dokumentasi Kegiatan</h1>
+            <div class="grid grid-cols-3 gap-4">
+                <div className='space-y-4'>
+                    <img src={Img1} alt="" className='w-full h-80 object-cover'/>
+                    <img src={Img2} alt="" className='w-full h-80 object-cover'/>
+                </div>
+                <div className='space-y-4'>
+                    <img src={Img3} alt="" className='w-full h-50 object-cover'/>
+                    <img src={Img6} alt="" className='w-full h-50 object-cover'/>
+                    <img src={Img5} alt="" className='w-full h-80 object-cover'/>
+                </div>
+                <div className='space-y-4'>
+                    <img src={Img4} alt="" className='w-full h-80 object-cover'/>
+                    <img src={Img7} alt="" className='w-full h-80 object-cover'/>
+                </div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default GaleryAbout
--- src/Components/EducationPage/AchivementEdu.jsx
+++ src/Components/EducationPage/AchivementEdu.jsx
@@ -33,8 +33,12 @@ const AchivementEdu = () => {
                 </div>
             </div>
             <div>
-                <img src={Image} alt="" />
+                <img src={Image} alt="foto" />
             </div>
+        </div>
+        <div className='text-center mt-20 space-y-4'>
+            <p>Bersama Kita Pilah, Bersama Kita Jaga Bandar Khalipah.</p>
+            <h2 className='text-xl font-bold'>Bank Sampah Bandar Khalipah bukan hanya tempat menyetorkan sampah,<br className='hidden md:block'/> tetapi juga ruang tumbuhnya kesadaran warga akan pentingnya kebersihan dan cinta lingkungan. <br className='hidden md:block'/>Di sini, warga diajak memilah sampah dari rumah dan melihat nilai dari barang bekas, membangun <br className='hidden md:block'/>budaya yang lebih peduli pada alam.</h2>
         </div>
     </section>
   )
