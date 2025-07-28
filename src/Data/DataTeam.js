378124f255ad95aae2aef62620e83ca5cc1c4de7
YusufKaisar <ykaisar10@gmail.com>
2025-07-28 19:26:41 +0700

    tim

--- src/Data/DataTeam.js
+++ src/Data/DataTeam.js
@@ -4,14 +4,7 @@ import img3 from "../assets/images/aboutpage/org3.webp"
 import img4 from "../assets/images/aboutpage/org4.webp"
 
 const dataTeam = [
-    {id:1, image: img1, name:"Bu SONY", role:"Focal Point", desc:"Beliau merupakan Focal Point utama sekaligus inisiator berdirinya Bank Sampah di Desa Bandar Khalipah. Dengan kepedulian tinggi terhadap isu lingkungan dan kesadaran akan pentingnya pengelolaan sampah yang berkelanjutan, beliau memulai inisiatif ini dari tingkat komunitas, mengajak warga untuk memilah dan menyetorkan sampah dengan sistem yang terorganisir."},
-    {id:2, image: img2, name:"budi", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:3, image: img3, name:"ahmad", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:4, image: img4, name:"rifki", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:5, image: img4, name:"surya", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:6, image: img4, name:"padil", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:7, image: img4, name:"pahmi", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
-    {id:8, image: img4, name:"said", role:"CEO", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."},
+    {id:1, image: img1, name:"SONY", role:"Focal Point", desc:"Focal point dan inisiator Bank Sampah di Desa Bandar Khalipah. Bertanggung jawab menggerakkan warga, melakukan sosialisasi, serta mendampingi proses penimbangan dan penukaran sampah."},
 ]
 
 export default dataTeam
