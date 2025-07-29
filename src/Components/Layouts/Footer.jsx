caa5ee4097acbea73afa48cb64eba8266308eaf2
YusufKaisar <ykaisar10@gmail.com>
2025-07-29 15:13:53 +0700

    update footer

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import logo from '/public/logo1.svg'
 
 const Footer = () => {
 
@@ -13,17 +14,16 @@ const Footer = () => {
 
     const moreInfo = [
         {label: "Jl. Besar Bandar Khalipah, Desa Bandar Khalipah, Kecamatan Percut Sei Tuan, Kabupaten Deli Serdang, Sumatera Utara, Indonesia."},
-        {label: "banksampahbandarkhalipah@gmail.com"},
+        {label: "banksampahbandarkhalipah @gmail.com"},
         {label: "+628098980898"},
     ]
     
   return (
     <section className='p-10 rounded-t-2xl bg-[#0B1D51]'>
         <div className='bg-white container mx-auto section-spacing rounded-3xl shadow-2xl p-10 justify-between'>
-            <div className='grid md:grid-cols-2 md:gap-5'>
-                <div className='flex'>
+            <div className='flex-col flex md:flex-row gap-4 md:gap-6'>
                     <div className='space-y-4'>
-                        <h1 className='text-2xl font-bold'>LOGO</h1>
+                        <img src={logo} alt="" className='w-20'/>
                         <p className=''>Ayo gabung bersama kami, kumpulkan sampah dan jaga lingkungan.</p>
                     </div>
                     <div className='space-y-4'>
@@ -40,7 +40,6 @@ const Footer = () => {
                             ))}
                         </ul>
                     </div>
-                </div>
                 <div className='space-y-4'>
                     <h2>Kontak</h2>
                     <ul className='space-y-2'>
@@ -52,7 +51,7 @@ const Footer = () => {
             </div>
             <div>
                 <hr className="border-t border-gray-300 mt-18 mb-6" />
-                <p className='text-center'>Copyright &copy; 2025 Bank Sampah Bandar Khalipah</p>
+                <p className='text-center font-bold'>Copyright &copy; 2025 Bank Sampah Bandar Khalipah</p>
             </div>
         </div>
 
